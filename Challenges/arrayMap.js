bb7d6a71c0552905f34e8b4235bd37be842e3a5f
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2024-06-07 03:44:46 +0200

    modifying arrayMap to handle negative numbers

--- Challenges/arrayMap.js
+++ Challenges/arrayMap.js
@@ -1,22 +1,30 @@
 function getSubArray(arr, target) {
     let sum = 0;
-    let startIndex = 0;
+
+    const sumMap = new Map();
+    sumMap.set(0, -1);
 
     for (let currIndex = 0; currIndex < arr.length; currIndex++) {
         sum += arr[currIndex];
-        while (sum > target) {
-            sum -= arr[startIndex];
-            startIndex++;
-        }
-        if (sum === target) {
+        
+        if (sumMap.has(sum - target)) {
             return true;
+          }
+
+        if (!sumMap.has(sum)) {
+            sumMap.set(sum, currIndex);
+          }
         }
-    }
+
+
     return false;
 }
 
 const args = process.argv.slice(2);
 const arr = args.slice(0, args.length - 1).map(Number);
+console.log(arr);
 const target = Number(args[args.length - 1]);
+console.log(target);
+
 
 console.log(getSubArray(arr, target));
